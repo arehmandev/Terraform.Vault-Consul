4cf87a77f432c2f729bdb3b46e619ecb1ef0138e
Abdul Rehman <arehmandev@outlook.com>
2017-06-20 20:30:00 +0100

    Cleaned up variables

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "Disclaimer" {
+  value = "Please wait a minute or so before going to the UI urls listed below"
+}
+
 output "loadbalancer_url_consul_servers" {
   value = "${module.ELB.servers_elb_dns_name}:8500/ui"
 }
